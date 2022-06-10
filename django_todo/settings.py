41dde747c40b0ebbe1fe7478e3897dc3460107e4
CCrocilla <cla.crocilla@gmail.com>
2022-06-10 22:38:48 +0000

    Fix allowed Hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app-hello-django-project.herokuapp.com']
 
 
 # Application definition
