ea373e12d4185e8618e423f3ec6c5f8f43349fe3
CCrocilla <cla.crocilla@gmail.com>
2022-06-10 00:55:13 +0000

    Prepare to Deploy to Heroku

---
+++ todo/views.py
@@ -0,0 +1,69 @@
+""" Imports """
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
+
+# Create your views here.
+# def say_hello(request):
+#     return HttpResponse("Hello!")
+
+
+def get_todo_list(request):
+    """Call the HTML file present in the template folder and display it """
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    """ Add Items """
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+
+        # Using the Forms in Django we don't need to express name 
+        # and done but they will be take automatically following 
+        # the new configuration
+
+        # name = request.POST.get('item_name')
+        # done = 'done' in request.POST
+        # Item.objects.create(name=name, done=done)
+        # return redirect('get_todo_list')
+
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    """Edit Items """
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
